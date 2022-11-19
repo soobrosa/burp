c697d4c0d63ebb5c26faa8e28c6234be35bf29c2
Daniel Molnar <soobrosa@gmail.com>
2022-11-19 16:21:09 +0100

    Organize the mess.

---
+++ 0_code/hol_menuzzek.py
@@ -0,0 +1,22 @@
+# https://github.com/soobrosa/slurp/tree/main
+
+# should run on Sundays, the day before weekly menu change
+
+from datetime import datetime, timedelta
+
+import urllib.request, urllib.error, urllib.parse
+
+def save_page(url, filename):
+
+  response = urllib.request.urlopen(url)
+  webContent = response.read().decode('windows-1250')
+  f = open(filename, 'w')
+  f.write(webContent)
+  f.close
+
+day_names = ['vasarnap', 'szombat', 'pentek', 'csutortok', 'szerda', 'kedd', 'hetfo']
+
+if datetime.today().weekday() == 6:
+  for day in range(0, 7):
+    filename = ('../1_html/' + datetime.today() - timedelta(days=day)).strftime('%Y-%m-%d') + '.html'
+    save_page('http://www.holmenuzzek.hu/' + day_names[day] + '.htm', filename)
